cd2e5ce21ebbb0dd4eb400a31e2545422ab73335
Prasenjit Saha <prasenjitsaha0110@gmail.com>
2025-11-30 12:20:09 +0530

    minor change

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -182,7 +182,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
   };
   //to be deleted later
@@ -257,7 +257,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     };
 
